b6cc2ec07cb786124e53e5ea9f9a28d068350a71
Jeff Schomay <schomay@adobe.com>
2019-09-26 17:48:44 -0700

    set up some metalsmith plugins

--- index.js
+++ index.js
@@ -3,17 +3,29 @@ var collections = require("metalsmith-collections");
 var markdown = require("metalsmith-markdown");
 var layouts = require("metalsmith-layouts");
 var permalinks = require("metalsmith-permalinks");
+var watch = require("metalsmith-watch");
+var metallic = require("metalsmith-metallic");
 
 Metalsmith(__dirname)
+    .source("./src")
+    .destination("./docs")
     .metadata({
         title: "My Static Site & Blog",
         description: "It's about saying »Hello« to the World.",
         generator: "Metalsmith",
         url: "http://www.metalsmith.io/"
     })
-    .source("./src")
-    .destination("./docs")
     .clean(true)
+    .use(
+        watch({
+            paths: {
+                "${source}/**/*": true,
+                "layouts/**/*": "**/*.md"
+            },
+            livereload: true
+        })
+    )
+    .use(metallic())
     .use(collections({ posts: "posts/*.md" }))
     .use(markdown())
     .use(permalinks({ relative: false }))
